f96c14418bf25cf9f0fe9010f619784b0b91a578
Edo Freriks <edofre@gmail.com>
2017-04-13 22:49:27 +0200

    Add float thead to main sitecontroller

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -100,6 +100,13 @@ class SiteController extends Controller
                 'composer' => 'https://packagist.org/packages/edofre/yii2-ckeditor',
                 'version'  => 'V1.0.1',
             ],
+            [
+                'name'     => 'yii2-float-thead',
+                'link'     => '/float-thead',
+                'github'   => 'https://github.com/Edofre/yii2-float-thead',
+                'composer' => 'https://packagist.org/packages/edofre/yii2-float-thead',
+                'version'  => 'V1.0.2',
+            ],
         ];
 
         return $this->render('index', ['packages' => $packages]);
